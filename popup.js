86355b71822a8b04e1014533d2047fe8d346ec48
Wei Shi <weishi@stanford.edu>
2013-05-04 21:28:54 -0700

    Added collapse/expand icon; Set default to collapse

--- popup.js
+++ popup.js
@@ -6,14 +6,17 @@ function parseDate(input) {
 };
 
 
+/* Google analytics */
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-40619774-1']);
 _gaq.push(['_trackPageview']);
 
 (function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = 'https://ssl.google-analytics.com/ga.js';
-          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      var ga = document.createElement('script'); 
+      ga.type = 'text/javascript'; ga.async = true;
+      ga.src = 'https://ssl.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0];
+      s.parentNode.insertBefore(ga, s);
 })();
 
 document.addEventListener('DOMContentLoaded', function () {
